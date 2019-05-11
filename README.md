6e2117b4f615ca39af59d56d886a000b9b830ffa
adesaegher <adesaegher@gmail.com>
2019-05-11 12:48:07 +0000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# flogo-components
+My collection of custom built flogo components
+
+## Components
+
+### Activities
+* [minios3](activity/minios3): Upload or Download files from Minio Simple Storage Service (S3)
---
+++ activity/minios3/activity.go
@@ -0,0 +1,173 @@
+// Package amazons3 uploads or downloads files from minio Simple Storage Service (S3)
+package minios3
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/minio/minio-go"
+)
+
+const (
+	ivAction             = "action"
+	ivS3Endpoint         = "S3Endpoint"
+	ivAwsAccessKeyID     = "awsAccessKeyID"
+	ivAwsSecretAccessKey = "awsSecretAccessKey"
+	ivUseSSL             = "useSSl"
+	ivAwsRegion          = "awsRegion"
+	ivS3BucketName       = "s3BucketName"
+	ivLocalLocation      = "localLocation"
+	ivS3Location         = "s3Location"
+	ivS3NewLocation      = "s3NewLocation"
+	ovResult             = "result"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-minios3")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// Get the action
+	action := context.GetInput(ivAction).(string)
+	S3Endpoint := context.GetInput(ivS3Endpoint).(string)
+	awsRegion := context.GetInput(ivAwsRegion).(string)
+	useSSl := context.GetInput(ivUseSSl).(bool)
+	s3BucketName := context.GetInput(ivS3BucketName).(string)
+	// localLocation is a file when uploading a file or a directory when downloading a file
+	localLocation := context.GetInput(ivLocalLocation).(string)
+	s3Location := context.GetInput(ivS3Location).(string)
+	s3NewLocation := context.GetInput(ivS3NewLocation).(string)
+
+	// AWS Credentials, only if needed
+	var awsAccessKeyID, awsSecretAccessKey = "", ""
+	if context.GetInput(ivAwsAccessKeyID) != nil {
+		awsAccessKeyID = context.GetInput(ivAwsAccessKeyID).(string)
+	}
+	if context.GetInput(ivAwsSecretAccessKey) != nil {
+		awsSecretAccessKey = context.GetInput(ivAwsSecretAccessKey).(string)
+	}
+
+	// See which action needs to be taken
+	var s3err error
+	switch action {
+	case "download":
+		s3err = downloadFileFromS3(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion, localLocation, s3Location, s3BucketName)
+	case "upload":
+		s3err = uploadFileToS3(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion, localLocation, s3Location, s3BucketName)
+	case "delete":
+		s3err = deleteFileFromS3(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion, s3Location, s3BucketName)
+	case "copy":
+		s3err = copyFileOnS3(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion, s3Location, s3BucketName, s3NewLocation)
+	}
+	if s3err != nil {
+		// Set the output value in the context
+		context.SetOutput(ovResult, s3err.Error())
+		return true, s3err
+	}
+
+	// Set the output value in the context
+	context.SetOutput(ovResult, "OK")
+
+	return true, nil
+}
+
+// Function to download a file from an S3 bucket
+func downloadFileFromS3(S3Endpoint string, awsAccessKeyID string, awsSecretAccessKey string, useSSL bool, awsRegion string, directory string, s3Location string, s3BucketName string) error {
+	// Create an instance of the Minio Client
+        minioClient, err := minio.New(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion)
+        if err != nil {
+                return err
+        }
+
+	// Create a new temporary file
+	f, err := os.Create(filepath.Join(directory, s3Location))
+	if err != nil {
+		return err
+	}
+
+	// Download the file to disk
+        _, err = minioClient.FGetObject(s3BucketName, objectName, f, minio.GetObjectOptions{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to delete a file from an S3 bucket
+func deleteFileFromS3(S3Endpoint string, awsAccessKeyID string, awsSecretAccessKey string, useSSL bool, awsRegion string, s3Location string, s3BucketName string) error {
+	// Create an instance of the Minio Client
+        minioClient, err := minio.New(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion)
+        if err != nil {
+                return err
+        }
+
+	// Delete the file from S3
+	_, err := err = minioClient.RemoveObject(s3BucketName, s3Location)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to upload a file from an S3 bucket
+func uploadFileToS3(S3Endpoint string, awsAccessKeyID string, awsSecretAccessKey string, useSSL bool, awsRegion string, localFile string, s3Location string, s3BucketName string) error {
+	// Create an instance of the Minio Client
+        minioClient, err := minio.New(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion)
+        if err != nil {
+                return err
+        }
+
+	// Upload the file
+	_, err = minioClient.FPutObject(s3BucketName, s3Location, localfile, minio.PutObjectOptions{});
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to copy a file in an S3 bucket
+func copyFileOnS3(S3Endpoint string, awsAccessKeyID string, awsSecretAccessKey string, useSSL bool, awsRegion string, s3Location string, s3BucketName string, s3NewLocation string) error {
+	// Create an instance of the Minio Client
+        minioClient, err := minio.New(S3Endpoint, awsAccessKeyID, awsSecretAccessKey, useSSL, awsRegion)
+        if err != nil {
+                return err
+        }
+
+	// Prepare the copy object
+        src := minio.NewSourceInfo(s3BucketName, s3BucketName,, nil)
+        dst, err := minio.NewDestinationInfo(s3BucketName, s3NewLocation, nil, nil)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+
+	// Copy the object
+	_, err := minioClient.CopyObject(dst, src)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
