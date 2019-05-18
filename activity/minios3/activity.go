3b26b9a5f5ce0152af0778dc54c7cbb3cea52661
adesaegher <adesaegher@gmail.com>
2019-05-18 13:14:38 +0000

    ...

--- activity/minios3/activity.go
+++ activity/minios3/activity.go
@@ -48,7 +48,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	action := context.GetInput(ivAction).(string)
 	s3Endpoint := context.GetInput(ivS3Endpoint).(string)
 	awsRegion := context.GetInput(ivAwsRegion).(string)
-	useSsl := toBool(context.GetInput(ivUseSsl))
+	useSsl, _ := toBool(context.GetInput(ivUseSsl))
 	s3BucketName := context.GetInput(ivS3BucketName).(string)
 	// localLocation is a file when uploading a file or a directory when downloading a file
 	localLocation := context.GetInput(ivLocalLocation).(string)
